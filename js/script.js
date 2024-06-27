604a8edeca5cf8afcd0ea7301e586f44743c50c0
EndriuK <157508097+EndriuK@users.noreply.github.com>
2024-06-27 15:53:34 +0200

    [UPD] fine form

--- js/script.js
+++ js/script.js
@@ -1,8 +1,16 @@
 //inizializzo un contatore
 
 for (let i = 1; i <= 100; i++) {
-  //controllo se divisibile per 3 o 5
+  //controllo che i sia multiplo di 3 o 5
   if (i % 3 == 0 && i % 5 == 0) {
     console.log("FizzBuzz");
+    //controllo che i sia multiplo di 3
+  } else if (i % 3 == 0) {
+    console.log("Fizz");
+    //controllo che i sia multiplo di 5
+  } else if (i % 5 == 0) {
+    console.log("Buzz");
+  } else {
+    console.log(i);
   }
 }
